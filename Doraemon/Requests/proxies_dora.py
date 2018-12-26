a7d2d9bd5e585a94311c0bc02ba350d442235da8
MaskAdam <wychengpublic@163.com>
2018-12-26 13:58:49 +0800

    set default proxies

--- Doraemon/Requests/proxies_dora.py
+++ Doraemon/Requests/proxies_dora.py
@@ -1,9 +1,10 @@
 import re
 import logging
-from Doraemon.Requests import requests_dora
+from Doraemon.Requests import requests_dora, settings
 import time
 import requests
 from bs4 import BeautifulSoup
+
 
 def get_data5u_proxies(api=None):
     url = "http://api.ip.data5u.com/dynamic/get.html?order=53b3de376027aa3f699dc335d2bc0674&sep=3"
@@ -34,7 +35,20 @@ def get_proxies(proxy):
     return proxies
 
 
-def get_proxies_fr_pool(pool):
+def set_pool(pool):
+    '''
+    should be invoked before get_proxies_fr_pool
+    :param pool:
+    :return:
+    '''
+    settings.POOL = pool
+
+
+def get_proxies_fr_pool():
+    if len(settings.POOL) == 0:
+        logging.warning("the POOL is dry...")
+        raise Exception
+    pool = settings.POOL
     len_pool = len(pool)
     index = int(time.time() % len_pool)
     proxy = pool[index]
@@ -49,32 +63,19 @@ def loc_proxies_ipv4(proxies):
         logging.warning("failed to connect to geolocation url ...")
 
 
-def detail_proxies(proxies):
+def detail_proxies_ipv6(proxies):
     test_res = requests_dora.try_best_2_get("http://ipv6.baidu.com/s?wd=ip", max_times=2, proxies=proxies, timeout=20)
     if test_res is not None and test_res.status_code == 200:
         soup = BeautifulSoup(test_res.text, "lxml")
         ip_detail = soup.select_one("div.op-ip-detail")
         if ip_detail is not None:
-            logging.warning(ip_detail.get_text())
+            logging.warning(ip_detail.get_text().strip())
     else:
         logging.warning("failed to connect to geolocation url ...")
     ""
 
 if __name__ == "__main__":
-    pool = [
-        "168.235.109.152:15028",
-        "168.235.109.152:15027",
-        "168.235.109.152:15026",
-        "168.235.109.152:15025",
-        "168.235.109.152:15024",
-        "168.235.109.152:15023",
-        "168.235.109.152:15022",
-        "168.235.109.152:15021",
-        "168.235.109.152:15020",
-        "168.235.109.152:15019",
-        "91.208.149.215:8000",
-    ]
     while True:
-        proxies = get_proxies_fr_pool(pool)
-        detail_proxies(proxies)
+        proxies = get_proxies_fr_pool()
+        detail_proxies_ipv6(proxies)
         time.sleep(1)
