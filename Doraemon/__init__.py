d7c71786b115caa920ef30f7114fa39d2555087f
MaskAdam <wychengpublic@163.com>
2018-12-26 13:43:57 +0800

    -

--- Doraemon/__init__.py
+++ Doraemon/__init__.py
@@ -1,4 +1,5 @@
 from Doraemon.Requests import chrome_dora
 from Doraemon.Requests import requests_dora
+from Doraemon.Requests import proxies_dora
 from Doraemon.OnlineSearch import google_KG
 from Doraemon.OnlineSearch import google_translator
