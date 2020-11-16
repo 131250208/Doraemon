a90b800b0de5afce4200ad0e8d5ec3b34233d639
MaskAdam <wychengpublic@163.com>
2020-11-16 18:06:10 +0800

    version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/131250208/Doraemon'
 EMAIL = 'wangyucheng@iie.ac.cn'
 AUTHOR = 'Andy Wong'
 REQUIRES_PYTHON = '>=3.5.0'
-VERSION = 3.4
+VERSION = 3.5
 
 # What packages are required for this module to be executed?
 REQUIRED = [
